ece0dd323356c1c8cf6e98ba3ec68023470b5b7a
pedroplays2020 <102863935+pedroplays2020@users.noreply.github.com>
2022-04-25 19:55:25 +0100

    Add files via upload

---
+++ program/program/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Carro : veiculo
+    {
+        //polimorfismo: dois ou mais metodos com os mesmo nome,
+        // mas com uma lista de parametros diferentes.
+        //
+        public int velocidade;
+        string cor;
+    }
+}
